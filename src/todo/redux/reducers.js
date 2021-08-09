3bb4f9303c0fc2a73b44786037a02edf51144861
Erkin303 <maxmudjonoverkin55@gmail.com>
2021-08-09 22:04:38 +0300

    biron bir ozgartirishni yozish

--- src/todo/redux/reducers.js
+++ src/todo/redux/reducers.js
@@ -1,27 +1,60 @@
 const initialState = {
     value: "",
+    value3: "",
     tasks: [
-        { main: "Task 1" },
-        { main: "Task 2" },
-        { main: "Task 3" }
+        { main: "Task 1", cmoplete: true, edit: false, value2: "" },
+        { main: "Task 2", cmoplete: true, edit: false, value2: "" },
+        { main: "Task 3", cmoplete: false, edit: false, value2: "" }
     ]
 }
 
-const reducer = (state = initialState, action) => {
+const Reducer = (state = initialState, action) => {
     switch (action.type) {
+        // asosiy input uchun
         case "TYPING": return { ...state, value: action.payload };
 
-        case "ADD_TASK": return { ...state, tasks: [...state.tasks, { main: action.payload }], value: "" };
+        // kichik cardlarni inputi un
+        case "TYPINGG": return { ...state, value3: action.payload };
 
+        // ma'lumot qo'shish un
+        case "ADD_TASK": return { ...state, tasks: [...state.tasks, { main: action.payload, cmoplete: true }], value: '' };
+
+        // o'chirish un
         case "DELETE_TASK":
-            let tasks = state.tasks;
+            let tasks = [...state.tasks];
             tasks.splice(action.payload, 1);
             return { ...state, tasks };
 
+        // hammasini ochirish un
+        case "CLEAR_ALL":
+            let taskss = [...state.tasks];
+            taskss.splice(action.payload, action.payload.length);
+            return { ...state, tasks: taskss, value: "" };
+
+        // soz ni ustiga chizish un
+        case "CHIZIQ":
+            let chiziqQoyish = [...state.tasks];
+            chiziqQoyish[action.payload].cmoplete = !chiziqQoyish[action.payload].cmoplete;
+            return { ...state, chiziqQoyish }
+
+        // so'zlarni o'zgartirish un
+        case "EDIT_TASK":
+            let editing = [...state.tasks];
+            editing[action.payload].edit = true;
+            state.value3 = editing[action.payload].main;
+            return { ...state, editing };
+
+
+        // o'zgartirilgan so'zlarni saqlash uchun
+        case "SAVE_TASK":
+            let name1 = [...state.tasks];
+            name1[action.payload].edit = false;
+            name1[action.payload].main = state.value3;
+            return { ...state, name1, value3: "" };
 
         default: return state;
     }
 
 
 }
-export default reducer;
+export default Reducer;
--- src/todo/redux/store.js
+++ src/todo/redux/store.js
@@ -1,9 +1,9 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
-import reducer from './reducers';
+import Reducer from './reducers';
 
 const middleware = [thunk];
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(Reducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
